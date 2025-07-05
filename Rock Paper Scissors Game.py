e2a379366782d8c92fd09d1f10eb83b109935409
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-07-05 23:28:24 +0600

    Update and rename rock paper scissors game.py to Rock Paper Scissors Game.py

--- Rock Paper Scissors Game.py
+++ Rock Paper Scissors Game.py
@@ -1,10 +1,7 @@
 #ROCK PAPER SCISSORS game
 import random
 
-
 options=("rock","paper","scissors") # A tuple of options
-
-
 
 score_pl=0
 score_com=0
@@ -13,42 +10,34 @@ total_played=0
 playing=True
 
 while playing:
-    player=input("ROCK/ PAPER/ SCISSORS? : ").lower()
 
+    player=input("ROCK/ PAPER/ SCISSORS? : ").lower()
     computer= random.choice(options)
 
     while player not in options:
         player=input("Idiot!! Enter ROCK/ PAPER/ SCISSORS? : ").lower()
+
     total_played+=1
+
+    print(f"You: {player}")
+    print(f"Computer: {computer}")
+    print("----------------------")
+    
     if player==computer:
-        print(f"You: {player}")
-        print(f"Computer:{computer}")
-        print("----------------------")
         print("DRAW!!")
             
     elif player=="rock" and computer=="scissors":
-        print(f"You: {player}")
-        print(f"Computer:{computer}")
-        print("----------------------")
         print("You WIN!!!")
         score_pl+=1
 
     elif player=="paper" and computer=="rock":
-        print(f"You: {player}")
-        print(f"Computer:{computer}")
-        print("----------------------")
         print("You WIN!!!")
         score_pl+=1
 
     elif player=="scissors" and computer=="paper":
-        print(f"You: {player}")
-        print(f"Computer:{computer}")
-        print("----------------------")
         print("You WIN!!!")
         score_pl+=1
     else :
-        print(f"Computer:{computer}")
-        print("----------------------")
         print("You LOSE :( ")
         score_com+=1
      
